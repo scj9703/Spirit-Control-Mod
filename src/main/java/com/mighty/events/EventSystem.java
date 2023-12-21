09eec5807cdc74023321d67819022b12d92646a1
Mighty-S0715 <150207407+Mighty-S0715@users.noreply.github.com>
2023-12-21 10:43:47 -0500

    First Commands / Event set-up
    /sc help
    /sc is WIP but stubbed-out
    Packages for Commands/Events
    Additions to Main for set-up

---
+++ src/main/java/com/mighty/events/EventSystem.java
@@ -0,0 +1,15 @@
+package com.mighty.events;
+
+import com.mighty.zsspiritcontrol.zsspiritcontrol;
+
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.PlayerEvent.PlayerLoggedOutEvent;
+import cpw.mods.fml.common.gameevent.TickEvent;
+import cpw.mods.fml.common.gameevent.TickEvent.PlayerTickEvent;
+import cpw.mods.fml.relauncher.Side;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraftforge.event.entity.EntityEvent.EntityConstructing;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
+
+public class EventSystem {
+}
--- src/main/java/com/mighty/zsspiritcontrol/zsspiritcontrol.java
+++ src/main/java/com/mighty/zsspiritcontrol/zsspiritcontrol.java
@@ -1,10 +1,19 @@
 package com.mighty.zsspiritcontrol;
 
+import com.mighty.events.CommonProxy;
 
 import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLServerAboutToStartEvent;
+import cpw.mods.fml.common.event.FMLServerStartingEvent;
+import cpw.mods.fml.common.event.FMLServerStartedEvent;
+import cpw.mods.fml.common.event.FMLServerStoppingEvent;
+import cpw.mods.fml.common.event.FMLServerStoppedEvent;
+import cpw.mods.fml.common.SidedProxy;
 
 @Mod(modid = zsspiritcontrol.MODID, version = zsspiritcontrol.VERSION)
 public class zsspiritcontrol
@@ -12,10 +21,54 @@ public class zsspiritcontrol
     public static final String MODID = "ZSSpiritcontrol";
     public static final String VERSION = "1.0";
 
+    @SidedProxy(clientSide = "com.mighty.events.ClientProxy", serverSide = "com.mighty.events.CommonProxy")
+    public static CommonProxy proxy;
+
     @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
-        // some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+    public void fmlLifeCycleEvent(FMLPreInitializationEvent event) {
+        proxy.fmlLifeCycleEvent(event);
     }
+
+    @EventHandler
+    public void fmlLifeCycleEvent(FMLInitializationEvent event) {
+        proxy.fmlLifeCycleEvent(event);
+        // some example code
+        System.out.println("DIRT BLOCK2 >> "+Blocks.dirt.getUnlocalizedName());
+    }
+
+    @EventHandler
+    public void fmlLifeCycle(FMLPostInitializationEvent event) {
+        proxy.fmlLifeCycleEvent(event);
+    }
+
+    @EventHandler
+    public void fmlLifeCycle(FMLServerAboutToStartEvent event) {
+        proxy.fmlLifeCycleEvent(event);
+
+    }
+
+    @EventHandler
+    public void fmlLifeCycle(FMLServerStartingEvent event) {
+        proxy.fmlLifeCycleEvent(event);
+
+    }
+
+    @EventHandler
+    public void fmlLifeCycle(FMLServerStartedEvent event) {
+        proxy.fmlLifeCycleEvent(event);
+
+    }
+
+    @EventHandler
+    public void fmlLifeCycle(FMLServerStoppingEvent event) {
+        proxy.fmlLifeCycleEvent(event);
+
+    }
+
+    @EventHandler
+    public void fmlLifeCycle(FMLServerStoppedEvent event) {
+        proxy.fmlLifeCycleEvent(event);
+
+    }
+
 }
