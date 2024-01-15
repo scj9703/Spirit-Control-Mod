2c663474299815f6c8b99173590a7f882563dac4
somehussar <148253652+somehussar@users.noreply.github.com>
2024-01-15 11:45:56 +0100

    Readded `/sc help`

---
+++ src/main/java/com/mighty/zsspiritcontrol/commands/spiritcontrol/SubCommand_Help.java
@@ -0,0 +1,43 @@
+package com.mighty.zsspiritcontrol.commands.spiritcontrol;
+
+import com.mighty.zsspiritcontrol.commands.SubCommand;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.util.ChatComponentTranslation;
+import net.minecraft.util.ChatStyle;
+import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.util.IChatComponent;
+
+import java.util.List;
+
+public class SubCommand_Help extends SubCommand {
+    String[] commandList = {
+            "help - shows a list of commands",
+            "skills - displays all unlocked skill names and descriptions",
+            "equip <slotName> <skillName> - equips the selected skill onto your loadout",
+            "enable [player] - unlocks Spirit Control for a Player OP",
+            "disable [player] - locks Spirit Control for a Player OP",
+            "check [player] - shows a Player's Gauge, loadout, and all unlocked abilities OP",
+            "unlock <skillName> [player] - unlocks an ability OP",
+            "lock <skillName> [player] - removes an ability OP" };
+
+    @Override
+    public void processCommand(ICommandSender sender, String[] args) {
+        /**
+         * @TODO
+         * Change display
+         */
+        for (String s : commandList) {
+            sender.addChatMessage((new ChatComponentTranslation(s).setChatStyle(new ChatStyle().setColor(EnumChatFormatting.AQUA))));
+        }
+    }
+
+    @Override
+    public List<String> addTabCompletionOptions(ICommandSender sender, String[] args) {
+        return null;
+    }
+
+    @Override
+    public boolean isUsernameIndex(String[] args, int index) {
+        return false;
+    }
+}
