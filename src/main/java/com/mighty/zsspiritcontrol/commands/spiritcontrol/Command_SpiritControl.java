1b31e95a713c2230f7b691181305eb394db4d713
somehussar <148253652+somehussar@users.noreply.github.com>
2024-01-15 11:45:56 +0100

    Readded `/sc help`

--- src/main/java/com/mighty/zsspiritcontrol/commands/spiritcontrol/Command_SpiritControl.java
+++ src/main/java/com/mighty/zsspiritcontrol/commands/spiritcontrol/Command_SpiritControl.java
@@ -37,7 +37,7 @@ public class Command_SpiritControl extends CommandBase {
         }
     }
     public Command_SpiritControl(){
-//        subCommandMap.put("help", new SubCommand_Help());
+        subCommandMap.put("help", new SubCommand_Help());
 //        subCommandMap.put("skills", new SubCommand_Skills());
 //        subCommandMap.put("enable", new SubCommand_Enable().addPerms(EnumPermission.SPIRITCONTROL_TOGGLE, EnumPermission.SPIRITCONTROL_ENABLE));
 //        subCommandMap.put("disable", new SubCommand_Disable().addPerms(EnumPermission.SPIRITCONTROL_TOGGLE, EnumPermission.SPIRITCONTROL_DISABLE));
