9b5cd49455eb1ab2fbe715f58bae6a7b2f4b8642
somehussar <148253652+somehussar@users.noreply.github.com>
2024-01-15 12:14:17 +0100

    Readded `/sc skills` and `/sc check`

---
+++ src/main/java/com/mighty/zsspiritcontrol/commands/spiritcontrol/SubCommand_Check.java
@@ -0,0 +1,81 @@
+package com.mighty.zsspiritcontrol.commands.spiritcontrol;
+
+import com.mighty.zsspiritcontrol.attack.Attack;
+import com.mighty.zsspiritcontrol.attack.PassiveAbility;
+import com.mighty.zsspiritcontrol.commands.SubCommand;
+import com.mighty.zsspiritcontrol.player.SCPlayer;
+import com.mighty.zsspiritcontrol.player.chat.ChatUtil;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.command.WrongUsageException;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.util.EnumChatFormatting;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SubCommand_Check extends SubCommand {
+
+    @Override
+    public void processCommand(ICommandSender sender, String[] args) {
+        if(!hasPerms(sender)){
+            throw new WrongUsageException("You don't have the correct permissions!");
+        }
+
+        if(args.length > 2){
+
+            throw new WrongUsageException("Too many arguments!");
+        }
+        if(args.length < 2){
+            throw new WrongUsageException("Not enough arguments!");
+        }
+
+        /**
+         * @TODO
+         * Revisit and rewrite this
+         */
+
+        EntityPlayerMP player = getPlayer(sender, args[1]);
+        SCPlayer extPlayer = SCPlayer.getPlayer(player);
+
+        double gauge = extPlayer.getCurrGauge();
+        double cap = extPlayer.getGaugeCapacity();
+        Attack superAttack1 = extPlayer.getSuperAttack1();
+        Attack superAttack2 = extPlayer.getSuperAttack2();
+        Attack ultimate = extPlayer.getUltimateAttack();
+        PassiveAbility passiveAbility = extPlayer.getPassiveAbility();
+        ArrayList<Attack> unlockedAttacks = extPlayer.getUnlockedAttacks();
+        ArrayList<PassiveAbility> unlockedPassives = extPlayer.getUnlockedPassives();
+
+        sender.addChatMessage(ChatUtil.getMessage("Checking player " + player.getCommandSenderName(), EnumChatFormatting.GRAY));
+        sender.addChatMessage(ChatUtil.getMessage("Spirit Gauge: " + gauge + " out of " + cap + " Spirit", EnumChatFormatting.GRAY));
+        sender.addChatMessage(ChatUtil.getMessage("Super1: " + superAttack1.getName() + " Super2: " + superAttack2.getName(), EnumChatFormatting.GRAY));
+        sender.addChatMessage(ChatUtil.getMessage("Ultimate: " + ultimate.getName() + " Passive: " + passiveAbility.getName(), EnumChatFormatting.GRAY));
+        String attackList = "Unlocked Attacks:";
+        String passiveList = "Unlocked Passives:";
+        for (Attack atk:unlockedAttacks){
+            attackList += " " + atk.getName();
+        }
+        for (PassiveAbility passive:unlockedPassives){
+            passiveList += " " + passive.getName();
+        }
+        sender.addChatMessage(ChatUtil.getMessage(attackList, EnumChatFormatting.GRAY));
+        sender.addChatMessage(ChatUtil.getMessage(passiveList, EnumChatFormatting.GRAY));
+
+    }
+
+    @Override
+    public List<String> addTabCompletionOptions(ICommandSender sender, String[] args) {
+        if(!hasPerms(sender)){
+            return null;
+        }
+
+        if(args.length == 2)
+            return getListOfStringMatchingLastWord(args, getPlayers());
+        return null;
+    }
+
+    @Override
+    public boolean isUsernameIndex(String[] args, int index) {
+        return index == 2;
+    }
+}
