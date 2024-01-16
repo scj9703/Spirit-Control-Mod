4a32fe2408a8978efc12f56d5b156972d7658c1d
somehussar <148253652+somehussar@users.noreply.github.com>
2024-01-16 02:00:21 +0100

    Made abilities inherit from the same class

---
+++ src/main/java/com/mighty/zsspiritcontrol/attack/Ability.java
@@ -0,0 +1,26 @@
+package com.mighty.zsspiritcontrol.attack;
+
+public class Ability {
+
+    protected String name;
+    protected String description;
+
+    protected Ability(String name, String description){
+        this.name = name;
+        this.description = description;
+    }
+
+    /**
+     * @return Ability's description as a string.
+     */
+    public String getDescription(){
+        return this.description;
+    }
+
+    /**
+     * @return Ability's name as a string.
+     */
+    public String getName(){
+        return this.name;
+    }
+}
