f9da959273f455947011796d0d32e32b79c27e55
somehussar <148253652+somehussar@users.noreply.github.com>
2024-01-17 16:45:37 +0100

    Added a DBC Wrapper class

---
+++ src/main/java/com/mighty/zsspiritcontrol/player/DBCPlayerWrapper.java
@@ -0,0 +1,27 @@
+package com.mighty.zsspiritcontrol.player;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.nbt.NBTTagCompound;
+
+public class DBCPlayerWrapper {
+    private EntityPlayer player;
+    private NBTTagCompound forgeTag;
+
+    public DBCPlayerWrapper(EntityPlayer player){
+        this.player = player;
+        this.forgeTag = player.getEntityData();
+    }
+
+    public boolean isFatigued(){
+        return this.getFatigue() != 0;
+    }
+
+    public int getFatigue(){
+        return forgeTag.getInteger("jrmcGodStrain");
+    }
+
+    public void setFatigue(int timeInSeconds){
+        this.forgeTag.setInteger("jrmcGodStrain", timeInSeconds*5);
+    }
+
+}
