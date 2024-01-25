967a5199357db8ca04c6e69b451e43f3d3850eb5
somehussar <148253652+somehussar@users.noreply.github.com>
2024-01-25 21:20:39 +0100

    Added proper decimal formatting to /spc check

--- src/main/java/com/mighty/spiritcontrol/command/spiritcontrol/SubCommand_Check.java
+++ src/main/java/com/mighty/spiritcontrol/command/spiritcontrol/SubCommand_Check.java
@@ -48,8 +48,10 @@ public class SubCommand_Check extends SCSubCommand {
         Set<Attack> unlockedUltimates = extPlayer.getUnlockedUltimates();
         Set<PassiveAbility> unlockedPassives = extPlayer.getUnlockedPassives();
 
+        DecimalFormat format = new DecimalFormat("#.##");
+
         sender.addChatMessage(MMParser.getFormat("<dark_aqua>Checking player: <gray>" + player.getCommandSenderName()));
-        sender.addChatMessage(MMParser.getFormat("<dark_aqua>Spirit Gauge: <gray><gauge></gray> out of <gray><max_gauge> (<percent>%)</gray> Spirit", "gauge", String.format("%.2f", gauge), "max_gauge", String.format("%.2f", cap), "percent", percent));
+        sender.addChatMessage(MMParser.getFormat("<dark_aqua>Spirit Gauge: <gray><gauge></gray> out of <gray><max_gauge> (<percent>%)</gray> Spirit", "gauge", format.format(gauge), "max_gauge", format.format(cap), "percent", percent));
         sender.addChatMessage(MMParser.getFormat("<dark_aqua>Super1: <gray><ability1></gray>, Super2: <gray><ability2></gray>", "ability1", superAttack1.getId(), "ability2", superAttack2.getId()));
         sender.addChatMessage(MMParser.getFormat("<dark_aqua>Ultimate: <gray><ability1></gray>, Passive: <gray><ability2></gray>", "ability1", ultimate.getId(), "ability2", passiveAbility.getId()));
         StringBuilder attackList = new StringBuilder("<dark_aqua>Unlocked Attacks:<gray>");
