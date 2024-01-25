7e1e508506b23d28be591c947ce6d11ba86bd337
somehussar <148253652+somehussar@users.noreply.github.com>
2024-01-25 22:36:23 +0100

    Version info update

--- src/main/java/com/mighty/spiritcontrol/SpiritControl.java
+++ src/main/java/com/mighty/spiritcontrol/SpiritControl.java
@@ -14,7 +14,7 @@ import org.apache.logging.log4j.Logger;
 public class SpiritControl
 {
     public static final String MODID = "spiritcontrol";
-    public static final String VERSION = "1.0.5";
+    public static final String VERSION = "1.0.6";
 
     public static Logger LOGGER;
 
