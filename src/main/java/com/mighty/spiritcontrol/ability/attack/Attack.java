1c22f2bd05c20e1ff4807577a2786d4d4a2cb02b
somehussar <148253652+somehussar@users.noreply.github.com>
2024-01-25 21:09:04 +0100

    Documentation

--- src/main/java/com/mighty/spiritcontrol/ability/attack/Attack.java
+++ src/main/java/com/mighty/spiritcontrol/ability/attack/Attack.java
@@ -105,7 +105,7 @@ public class Attack extends Ability {
 
         ex.removeSpirit(ex.getMaxBaseSpirit() * getCostModifier());
         MinecraftServer.getServer().getCommandManager().executeCommand(MinecraftServer.getServer(), "dbcspawnki "+type+" "+speed+" "+damage+" "+(effect ? 1 : 0)+" "+color+" "+"100 1 100 0 0 0 "+ex.player.getCommandSenderName());
-        ex.dbcPlayer.getNbt().setByte("jrmcFrng", (byte) 1);
+        ex.dbcPlayer.getNbt().setByte("jrmcFrng", (byte) 1); //Fixes a bug with ki attacks not being connected to the player
         ex.addChatMessage(ex.drawPrettyGauge());
     }
 }
